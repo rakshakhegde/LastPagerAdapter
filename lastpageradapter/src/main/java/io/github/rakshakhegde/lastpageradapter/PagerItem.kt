d5704a053e31cc4438985468bc198dcaf4a1a8e2
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-05-03 21:18:43 +0530

    cleanup pageritem

--- lastpageradapter/src/main/java/io/github/rakshakhegde/lastpageradapter/PagerItem.kt
+++ lastpageradapter/src/main/java/io/github/rakshakhegde/lastpageradapter/PagerItem.kt
@@ -1,8 +1,6 @@
 package io.github.rakshakhegde.lastpageradapter
 
 import android.databinding.ViewDataBinding
-import android.os.Parcelable
-import android.util.SparseArray
 import java.lang.ref.SoftReference
 
 /**
@@ -14,29 +12,12 @@ data class PagerItem(
 		val title: CharSequence?,
 		val width: Float
 ) {
-	init {
-		// Bind
-	}
 
 	var binding: ViewDataBinding?
 		get() = bindingRef?.get()
 		internal set(value) {
-			value?.root?.addOnAttachStateChangeListener()
 			bindingRef = SoftReference(value)
 		}
 
 	private var bindingRef: SoftReference<ViewDataBinding?>? = null
-
-	private var hierarchyState: SparseArray<Parcelable>? = null
-
-	internal fun saveHierarchyState() {
-		binding?.root?.apply {
-			hierarchyState = SparseArray()
-			saveHierarchyState(hierarchyState)
-		}
-	}
-
-	internal fun restoreHierarchyState() {
-		hierarchyState?.let { binding?.root?.restoreHierarchyState(it) }
-	}
 }
